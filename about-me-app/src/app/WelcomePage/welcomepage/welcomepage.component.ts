4447e56facd256f91af4f7ee1901521321dc5a89
SirRoxer <vtstiv@gmail.com>
2024-01-20 14:01:27 +0200

    Update
    Finish scrolldown icon.

--- about-me-app/src/app/WelcomePage/welcomepage/welcomepage.component.ts
+++ about-me-app/src/app/WelcomePage/welcomepage/welcomepage.component.ts
@@ -7,16 +7,19 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class WelcomepageComponent {
   @ViewChild('userBox') userBox: ElementRef | undefined;
+  detailsOpen: boolean = false;
 
   openinfo() {
     if (this.userBox) {
       this.userBox.nativeElement.classList.remove("hide-details");
+      this.detailsOpen = true;
     }
   }
 
   closeinfo() {
     if (this.userBox) {
       this.userBox.nativeElement.classList.add("hide-details");
+      this.detailsOpen = false;
     }
   }
 }
